d829dd578fb8c064569ef44441a9e2ada10f598a
Samuel <henrisamuel234@gmail.com>
2025-08-14 17:32:38 -0300

    acabados v1

--- src/calculadoraContagem/main.java
+++ src/calculadoraContagem/main.java
@@ -3,10 +3,26 @@ package calculadoraContagem;
 import java.util.Scanner;
 
 public class main {
-    public static void mains(String[] args) {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("DIGITE 1 PARA CONTAGEM DE ACABADOS %n DIGITE 2 PARA CONTAGEM DE COMPONENTES");
+        System.out.printf(" DIGITE 1 PARA CONTAGEM DE ACABADOS %n DIGITE 2 PARA CONTAGEM DE COMPONENTES %n");
+        int cont = sc.nextInt();
+
+        if (cont == 1) {
+
+            System.out.print("CODIGO DO PRODUTO: ");
+            String cod1 = sc.next();
+
+            System.out.print("NUMERO DE CAIXAS: ");
+            int caixa = sc.nextInt();
+
+            System.out.print("QUANTIDADE POR CAIXA: ");
+            int qntde = sc.nextInt();
+
+            int acabados = caixa * qntde;
+            System.out.printf("CODIGO: %s%n TOTAL = %d%n", cod1, acabados);
+        }
     }
 }
