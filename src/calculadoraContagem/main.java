9ae67133c5ded180d176ebc97aff4a4bac8fe835
Samuel <henrisamuel234@gmail.com>
2025-08-14 17:19:51 -0300

    Configurando pastas

--- src/calculadoraContagem/main.java
+++ src/calculadoraContagem/main.java
@@ -1,4 +1,12 @@
 package calculadoraContagem;
 
+import java.util.Scanner;
+
 public class main {
+    public static void mains(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("DIGITE 1 PARA CONTAGEM DE ACABADOS %n DIGITE 2 PARA CONTAGEM DE COMPONENTES");
+    }
 }
