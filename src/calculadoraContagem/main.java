9bc805acafd32f1ef012724a1e67520d1fc1b404
Samuel <henrisamuel234@gmail.com>
2025-08-15 12:01:45 -0300

    acabados v2

--- src/calculadoraContagem/main.java
+++ src/calculadoraContagem/main.java
@@ -22,9 +22,29 @@ public class main {
             int qntde = sc.nextInt();
 
             int acabados = caixa * qntde;
-            System.out.printf("CODIGO: %s%n TOTAL = %d%n", cod1, acabados);
-        } else if (cont == 2) {
-
+            System.out.println("CONTAGEM ACABADOS");
+            System.out.printf(" CODIGO: %s%n TOTAL = %d%n", cod1, acabados);
         }
+
+        else if (cont == 2) {
+
+            System.out.print("CODIGO DO PRODUTO: ");
+            String cod2 = sc.next();
+
+            System.out.println("QUANTIDADE DE 3 CAIXAS: ");
+            int cx1 = sc.nextInt();
+            int cx2 = sc.nextInt();
+            int cx3 = sc.nextInt();
+
+            System.out.print("NUMERO DE CAIXAS:");
+            int numc = sc.nextInt();
+
+            double divi = cx1 + cx2 + cx3 / 3;
+            double totalac = divi * numc;
+
+            System.out.printf(" CODIGO = %s%n TOTAL = %f.2");
+        }
+
+        sc.close();
     }
 }
